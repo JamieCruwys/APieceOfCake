604bd36fb0d4a67778e9b8f26bc2b10a52fe1074
Jamie Cruwys <me@jamiecruwys.co.uk>
2019-05-30 00:17:47 +0100

    Made Cake parcelable

--- app/src/main/java/uk/co/jamiecruwys/apieceofcake/api/Cake.kt
+++ app/src/main/java/uk/co/jamiecruwys/apieceofcake/api/Cake.kt
@@ -1,7 +1,11 @@
 package uk.co.jamiecruwys.apieceofcake.api
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Cake(
     val title: String?,
     val desc: String?,
     val image: String?
-)
+) : Parcelable
